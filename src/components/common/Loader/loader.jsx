9a9bcea7a62b7399f162dce53fd7c0883d0ce00b
sanjay18-r <sanjaydharsaan007@gmail.com>
2026-08-05 13:51:18 +0530

    updated the latest package.json

--- src/components/common/Loader/loader.jsx
+++ src/components/common/Loader/loader.jsx
@@ -2,7 +2,6 @@ import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { FaExclamationTriangle } from "react-icons/fa";
 import styles from "./Loader.module.css";
-
 import logo from "../../../assets/logos/logo-white.png";
 
 const Loader = ({ finishLoading }) => {
