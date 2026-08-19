69b24f37fb48ac7799ad95eb342373d40c3f6cbf
sanjay18-r <sanjaydharsaan007@gmail.com>
2026-08-19 10:31:25 +0530

    Fix team.js missing module imports and update phone number

--- src/data/team.js
+++ src/data/team.js
@@ -1,8 +1,6 @@
 import team1 from "../assets/images/team/team-1.jpg";
 import team2 from "../assets/images/team/team-2.jpg";
 import team3 from "../assets/images/team/team-3.jpg";
-import team4 from "../assets/images/team/team-4.jpg";
-import team5 from "../assets/images/team/team-5.jpg";
 
 const team = [
   {
